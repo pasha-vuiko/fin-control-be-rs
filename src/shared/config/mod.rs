48cd5e66e3b5cc5f82f8e190e536faa546e2fe17
pavlo-vuiko <pasha.vuiko@gmail.com>
2023-03-08 14:09:49 +0200

    chore(app): replace clap crate with envy crate

--- src/shared/config/mod.rs
+++ src/shared/config/mod.rs
@@ -1,34 +1,24 @@
-use clap::Parser;
+use serde::Deserialize;
 
 pub mod tracing;
 
-#[derive(Parser)]
-pub struct Config {
+#[derive(Deserialize, Debug)]
+pub struct AppConfig {
     // App
-    #[clap(long, env)]
     pub port: u16,
 
     // Redis
-    #[clap(long, env)]
     pub redis_config_sentinels: String,
-    #[clap(long, env)]
     pub redis_config_name: String,
-    #[clap(long, env)]
     pub redis_config_host: String,
-    #[clap(long, env)]
     pub redis_config_port: u16,
-    #[clap(long, env)]
     pub redis_ttl: u16, // seconds
 
     // Auth0
-    #[clap(long, env)]
     pub auth_auth0_domain: String,
-    #[clap(long, env)]
     pub auth_client_id: String,
-    #[clap(long, env)]
     pub auth_client_secret: String,
 
     // Database
-    #[clap(long, env)]
     pub database_url: String,
 }
--- src/shared/mods/redis/mod.rs
+++ src/shared/mods/redis/mod.rs
@@ -1,14 +1,13 @@
-use std::env;
+use crate::shared::config::AppConfig;
 
 pub mod redis_service;
 use crate::shared::mods::redis::redis_service::RedisService;
 
-pub async fn get_redis_service() -> RedisService {
-    let redis_host =
-        env::var("REDIS_CONFIG_HOST").expect("Env var 'REDIS_CONFIG_HOST' is not specified");
-    let redis_port =
-        env::var("REDIS_CONFIG_PORT").expect("Env var 'REDIS_CONFIG_PORT' is not specified");
-    let redis_uri = format!("redis://{}:{}", redis_host, redis_port);
+pub async fn get_redis_service(config: &AppConfig) -> RedisService {
+    let redis_uri = format!(
+        "redis://{}:{}",
+        config.redis_config_host, config.redis_config_port
+    );
 
     let redis_client = redis::Client::open(redis_uri).expect("Can't create Redis client");
     let redis_connection_manager = redis_client
