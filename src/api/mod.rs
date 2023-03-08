8e13717d622321be5b9f4e9435789ef4487ea7b3
pavlo-vuiko <pasha.vuiko@gmail.com>
2023-03-08 14:09:49 +0200

    chore(app): replace clap crate with envy crate

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,6 +1,8 @@
-use std::sync::Arc;
-use axum::Router;
 use axum::routing::get;
+use axum::Router;
+use std::{env, sync::Arc};
+
+use crate::shared::config::AppConfig;
 use crate::shared::mods::prisma::PrismaClient;
 use crate::shared::mods::redis::redis_service::RedisService;
 
@@ -9,13 +11,13 @@ mod customers;
 pub fn get_root_api_router(
     prisma_client: Arc<PrismaClient>,
     redis_service: Arc<RedisService>,
+    config: &AppConfig,
 ) -> Router {
     Router::new().route("/", get(index))
 }
 
 async fn index() -> String {
-    let app_ver = "0.1.0";
-    // let app_ver = env::var("CARGO_PKG_VERSION").unwrap();
+    let app_ver = env::var("CARGO_PKG_VERSION").unwrap();
 
     format!("App version: {}", app_ver)
 }
