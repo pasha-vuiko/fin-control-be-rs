5f62261b34e8c9bdfaab67f4aed3e0d10852357c
pavlo-vuiko <pasha.vuiko@gmail.com>
2023-03-27 21:21:34 +0300

    refactor(customers): refactor code

--- src/api/customers/structs/state.rs
+++ src/api/customers/structs/state.rs
@@ -1,5 +1,4 @@
 use crate::api::customers::customers_service::CustomersService;
-use crate::shared::config::AppConfig;
 use crate::shared::mods::auth::service::AuthService;
 use crate::shared::mods::redis::redis_service::RedisService;
 use axum::extract::FromRef;
@@ -7,10 +6,15 @@ use std::sync::Arc;
 
 #[derive(Clone)]
 pub struct CustomersApiState {
-    pub config: AppConfig,
     pub customers_service: CustomersService,
     pub redis_service: Arc<RedisService>,
     pub auth_service: Arc<AuthService>,
+}
+
+impl FromRef<CustomersApiState> for CustomersService {
+    fn from_ref(app_state: &CustomersApiState) -> CustomersService {
+        app_state.customers_service.clone()
+    }
 }
 
 impl FromRef<CustomersApiState> for Arc<RedisService> {
