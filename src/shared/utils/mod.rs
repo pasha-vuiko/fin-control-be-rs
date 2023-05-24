adbf5a3cc14a81b116d769f9c7738dbe7c2e7325
pavlo-vuiko <pasha.vuiko@gmail.com>
2023-05-24 19:36:58 +0300

    refactor(shared): refactor code in main

--- src/shared/utils/mod.rs
+++ src/shared/utils/mod.rs
@@ -2,8 +2,8 @@ use axum::http::{header, Request};
 
 pub fn get_bearer_token<B>(req: &Request<B>) -> Option<String> {
     let authorization_header = req.headers().get(header::AUTHORIZATION)?;
-    let bearer_token = authorization_header.to_str().ok()?;
-    let bearer_token = bearer_token.trim_start_matches("Bearer ");
+    let bearer_token = authorization_header.to_str().ok()?.to_string();
+    let token = bearer_token.trim_start_matches("Bearer ");
 
-    Some(bearer_token.to_string())
+    Some(token.to_string())
 }
